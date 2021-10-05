1fda94a8d4927ce4c46dc97582aad15b3f59f953
Andres C <69328357+andres-c94@users.noreply.github.com>
2021-10-05 13:14:22 -0500

    Add files via upload

---
+++ pryectos.js
@@ -0,0 +1,191 @@
+function minAmb(){
+    Swal.fire({
+
+    html: 
+            `
+            <h3>Formulario ministerio del ambiente</h3>
+            <div class="container-fluid">
+
+            <div class="row">
+
+              <div class="col-8">
+         
+              <iframe src="Landing TuArbol (1).pdf" width="100%" height="500rem" frameborder="0"></iframe><hr>
+
+              </div>
+
+
+              <div class="col-4">
+
+          
+
+              <p align="justify">Proyecto para el Ministerio de ambiente que permitiria a las personas realizar un registro de los arboles sembrados y su ubicacion geografica, para posteriormente poder consultar los datos (Ya no esta desplegada)</p>
+                  <hr>
+                  <p align="left" style="font-size: 13.5px;"><b>Rol:</b> Desarollo backend y frontend</p>
+                  <p align="left" style="font-size: 13.5px;"><b>Tecnologias usadas:</b> HTML, CSS, Bootstrap, Js, API REST ArcGIS, Survey 123 for ArcGIS </p>
+                  <p align="left" style="font-size: 13.5px;"><b>Empresa:</b> Esri Colombia</p><hr>
+                  
+               
+
+                  
+              
+
+
+           
+
+            </div>
+            </div>
+
+
+           
+            `,
+
+            //confirmButtonText: "Volver",
+            //confirmButtonColor: "#007ac2",
+            showConfirmButton: false,
+            //width: "80%",
+            //height="80%",
+            //footer:`${pais}`,
+            showCloseButton: true,
+            padding: "1px",
+            background: "#f9fafb",
+            grow: "fullscreen",
+            backdrop: true,
+
+        });
+
+
+
+
+
+}
+
+
+
+function qqsm(){
+
+
+
+
+
+
+    Swal.fire({
+
+        html: 
+                `
+                <h3>Quien quiere ser geografico</h3>
+                <div class="container-fluid">
+    
+                <div class="row">
+    
+                  <div class="col-8">
+                    <iframe src="https://geoapps.esri.co/QuienQuiereSerGeogr-ficoV2-main/" width="100%" height="500rem" frameborder="0"></iframe><hr>
+                  </div>
+    
+    
+                  <div class="col-4">
+    
+              
+    
+                  <p align="justify">Juego desarollado para el cierre de los eventos de la empresa Esri Colombia, parecido a "quien quiere ser millonario", esta conectado con Go to Webinar mediate su API, asi podemos traer los participantes del evento para elegir a uno al azar, tiene ayuda al publico, la cual pregunta a los asistentes y mustra un grafico con los resultados, 50/50, llamada a un amigo, entre otras funciones</p>
+                      <hr>
+                      <p align="left" style="font-size: 13.5px;"><b>Rol:</b> Desarollo backend: conexion con la API y llamados, Sistema de preguntas, Ayuda al publico y Llamada a un  amigo</p>
+                      <p align="left" style="font-size: 13.5px;"><b>Tecnologias usadas:</b> HTML, CSS, Bootstrap, Js, API REST GoToWebinar, Sweetalert 2, Chart.js, PHP, Json </p>
+                      <p align="left" style="font-size: 13.5px;"><b>Empresa:</b> Esri Colombia</p><hr>
+                      <a target="_blank" href="https://geoapps.esri.co/QuienQuiereSerGeogr-ficoV2-main/"> <button class="btn-modal" >Ir a la pagina</button> </a>
+                   
+    
+                      
+                  
+    
+    
+               
+    
+                </div>
+                </div>
+    
+    
+               
+                `,
+    
+                //confirmButtonText: "Volver",
+                //confirmButtonColor: "#007ac2",
+                showConfirmButton: false,
+                //width: "80%",
+                //height="80%",
+                //footer:`${pais}`,
+                showCloseButton: true,
+                padding: "1px",
+                background: "#f9fafb",
+                grow: "fullscreen",
+                backdrop: true,
+    
+            });
+
+
+}
+
+
+
+
+
+
+function galeria(){
+   
+
+    Swal.fire({
+
+        html: 
+                `
+                <h3>Galeria de mapas</h3>
+                <div class="container-fluid">
+    
+                <div class="row">
+    
+                  <div class="col-8">
+                    <iframe src="https://galeriamapas.esri.co" width="100%" height="500rem" frameborder="0"></iframe><hr>
+                  </div>
+    
+    
+                  <div class="col-4">
+    
+              
+    
+                  <p align="justify">Galeria de mapas para el evento anual de la empresa Esri Colombia, en la que los usuarios suben sus mapas y mediante un sistema de votacion de elige a los mejores, al final se muestra una grafica con los resultados de las votaciones y los jurados, se muestran los mapas ganadores de este año</p>
+                      <hr>
+                      <p align="left" style="font-size: 13.5px;"><b>Rol:</b> Desarollo backend y frontend (proyecto individual)</p>
+                      <p align="left" style="font-size: 13.5px;"><b>Tecnologias usadas:</b> HTML, CSS, Bootstrap, Js, API REST ArcGIS, Survey 123 for ArcGIS, Sweetalert 2, Chart.js, PHP </p>
+                      <p align="left" style="font-size: 13.5px;"><b>Empresa:</b> Esri Colombia</p><hr>
+                      <a target="_blank" href="https://galeriamapas.esri.co"> <button class="btn-modal" >Ir a la pagina</button> </a>
+                   
+    
+                      
+                  
+    
+    
+               
+    
+                </div>
+                </div>
+    
+    
+               
+                `,
+    
+                //confirmButtonText: "Volver",
+                //confirmButtonColor: "#007ac2",
+                showConfirmButton: false,
+                //width: "80%",
+                //height="80%",
+                //footer:`${pais}`,
+                showCloseButton: true,
+                padding: "1px",
+                background: "#f9fafb",
+                grow: "fullscreen",
+                backdrop: true,
+    
+            });
+    
+
+
+}
